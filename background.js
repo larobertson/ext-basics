37d1d5032f2ebf4e91bb958e58c3029fab473512
Laura <laura.robertson64@gmail.com>
2020-08-23 22:56:49 -0500

    send a message from frontend to backend

--- background.js
+++ background.js
@@ -1,8 +1,16 @@
 chrome.tabs.onActivated.addListener(tab => {
   chrome.tabs.get(tab.tabId, current_tab_info => {
     if(/^https:\/\/www\.google/.test(current_tab_info.url)) {
-      chrome.tabs.insertCSS(null, {file: './mystyles.css'});
       chrome.tabs.executeScript(current_tab_info.id, {file: './foreground.js'}, () => console.log('injected!!!'))
+      chrome.tabs.insertCSS(null, {file: './mystyles.css'});
     }
   });
 });
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === 'yo check the storage!!!') {
+    chrome.storage.local.get("password", value => {
+      console.log('value', value);
+    });
+  }
+})
--- foreground.js
+++ foreground.js
@@ -1,3 +1,22 @@
-console.log('this is the foreground');
+const first = document.createElement('button');
+first.innerText = "SET DATA";
+first.id = "first";
 
-document.querySelector('[alt="Google"]').classList.add('spinspinspin')
+const second = document.createElement('button');
+second.innerText = "SHOUTOUT TO BACKEND";
+second.id = "second";
+
+
+document.querySelector('body').appendChild(first);
+document.querySelector('body').appendChild(second);
+
+
+first.addEventListener('click', () => {
+  chrome.storage.local.set({"password": "123"})
+  console.log('log log log, I set data');
+})
+
+second.addEventListener('click', () => {
+  chrome.runtime.sendMessage({message: 'yo check the storage!!!'})
+  console.log('I sent the message');
+})
