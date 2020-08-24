aba1e2eed16d9fe84071e33f443a12e6b8828e7c
Laura <laura.robertson64@gmail.com>
2020-08-23 22:31:11 -0500

    set up tabs logic for foreground script to execute

--- background.js
+++ background.js
@@ -1,1 +1,7 @@
-console.log('background!!');
+chrome.tabs.onActivated.addListener(tab => {
+  chrome.tabs.get(tab.tabId, current_tab_info => {
+    if(/^https:\/\/www\.google/.test(current_tab_info.url)) {
+      chrome.tabs.executeScript(current_tab_info.id, {file: './foreground.js'}, () => console.log('injected!!!'))
+    }
+  });
+});
